34f153c289b8b9c98a6c78f03c8f01fd496a5284
SitanHuang <978494543.qq54@gmail.com>
2022-01-30 13:25:50 -0600

    fix quota=0

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < Sequel::Model(:users)
   def quota
     # 126kb > 505kb > 1.1mb > 2.0mb > 3.1mb > 4.4mb > 6.0mb > 7.9mb > 10.mb
     # 1       2       3       4       5       6       7       8       9
-    (self.status / 9) ** 2 * 10485760
+    ((self.status / 9.0) ** 2 * 10485760).floor
   end
 end
