16fabdb9d05af690cfba1b780a1f7ac1f3ae5165
SitanHuang <978494543.qq54@gmail.com>
2023-01-27 16:30:45 -0600

    version update alert 2

--- app/public/client/libs/index.js
+++ app/public/client/libs/index.js
@@ -1,6 +1,6 @@
-const RELEASE_DATE = 20230127;
+const RELEASE_DATE = 20230128;
 
-if (localStorage.last_release && localStorage.last_release != RELEASE_DATE)
+if (localStorage.last_release && localStorage.last_release != RELEASE_DATE.toString())
   alert(`TaskCentral update detected: ${localStorage.last_release} -> ${RELEASE_DATE}. Changes may apply. Browser cache clear recommended.`);
 
 localStorage.last_release = RELEASE_DATE;
