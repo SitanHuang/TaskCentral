b0983b2ecadaa4d8fe6a76071dd248002a538211
SitanHuang <978494543.qq54@gmail.com>
2023-08-30 11:06:51 -0700

    move snooze button

--- app/public/client/libs/index.js
+++ app/public/client/libs/index.js
@@ -1,13 +1,12 @@
-const RELEASE_DATE = '20230828';
+const RELEASE_DATE = '20230830';
 
 const RELEASE_NOTES = `
 
 ########## RELEASE NOTES ##########
-1. Added snooze feature & filters
-2. Added theme selection & auto dark mode
+1. Fulfill request to move snooze button
 `;
 
-//if (localStorage.last_release && localStorage.last_release != RELEASE_DATE.toString())
-//  alert(`TaskCentral update detected: ${localStorage.last_release} -> ${RELEASE_DATE}. Browser cache clear recommended.\n ${RELEASE_NOTES}`);
+if (localStorage.last_release && localStorage.last_release != RELEASE_DATE.toString())
+  alert(`TaskCentral update detected: ${localStorage.last_release} -> ${RELEASE_DATE}. Browser cache clear recommended.\n ${RELEASE_NOTES}`);
 
 localStorage.last_release = RELEASE_DATE;
