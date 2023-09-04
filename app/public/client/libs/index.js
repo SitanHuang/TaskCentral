b7f83dc0e8810bd681a5cfe350bd35d4e8f5e3a4
SitanHuang <978494543.qq54@gmail.com>
2023-09-03 20:11:09 -0700

    async prompts infrastructure

--- app/public/client/libs/index.js
+++ app/public/client/libs/index.js
@@ -3,12 +3,13 @@ const RELEASE_DATE = '20230903R1';
 const RELEASE_NOTES = `
 ########## RELEASE NOTES ##########
 1. Fix bug where thread blocking confirm dialog causes server syncs to fail (ie. when deleting tasks).
+2. Add async dialog infrastructure
 
 --------- PREVIOUS NOTES ----------
 1. Add Skill Groups in Metrics
 `;
 
 if (localStorage.last_release && localStorage.last_release != RELEASE_DATE.toString())
-  alert(`TaskCentral update detected: ${localStorage.last_release} -> ${RELEASE_DATE}. Browser cache clear recommended.\n ${RELEASE_NOTES}`);
+  ui_alert(`TaskCentral update detected: ${localStorage.last_release} -> ${RELEASE_DATE}. Browser cache clear recommended.\n ${RELEASE_NOTES}`);
 
 localStorage.last_release = RELEASE_DATE;
--- app/public/client/libs/ui/dialog.js
+++ app/public/client/libs/ui/dialog.js
@@ -1,17 +1,31 @@
 async function ui_confirm(message="") {
+  return await _ui_raise_dialog({ message, cancel: true });
+}
+
+async function ui_alert(message = "") {
+  await _ui_raise_dialog({ message, cancel: false });
+}
+
+async function _ui_raise_dialog({
+  message = "",
+  cancel  = true,
+  // TODO: input = false
+} = {}) {
+  $("#modal-ui-dialog .modal__btn.cancel").toggle(!!cancel);
+
   return new Promise(resolve => {
-    document.querySelector("#modal-ui-confirm .modal__content").textContent = message;
+    document.querySelector("#modal-ui-dialog .modal__content").textContent = message;
 
-    MicroModal.show('modal-ui-confirm');
+    MicroModal.show('modal-ui-dialog');
 
-    const primaryBtn = document.querySelector("#modal-ui-confirm .modal__btn.modal__btn-primary");
+    const primaryBtn = document.querySelector("#modal-ui-dialog .modal__btn.modal__btn-primary");
     primaryBtn.onclick = function () {
-      MicroModal.close('modal-ui-confirm');
+      MicroModal.close('modal-ui-dialog');
 
       resolve(true);
     };
 
-    const closeBtn = document.querySelector("#modal-ui-confirm .modal__close");
+    const closeBtn = document.querySelector("#modal-ui-dialog .modal__close");
     closeBtn.onclick = closeModalAndResolve;
 
     function closeModalAndResolve() {
