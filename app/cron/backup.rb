1af770c9737b377e93d9a07e7b375bddb8489876
SitanHuang <978494543.qq54@gmail.com>
2023-10-01 14:37:18 -0700

    auto backup mariadb

--- app/cron/backup.rb
+++ app/cron/backup.rb
@@ -1,10 +1,18 @@
 require 'open3'
+require 'shellwords'
 
-# Backup res to res.swp every 2 AM PDT (4 AM CDT)
-$CRON_SCHEDULER.cron "0 2 * * *" do
+def cron_backup_res
   current_file_folder = File.dirname(__FILE__)
   src = "#{current_file_folder}/../res"
   dst = "#{current_file_folder}/../res.swp"
+
+  credentials_path = File.expand_path('../../db.credentials', __FILE__)
+  credentials = JSON.parse(File.read(credentials_path))
+
+  escaped_username = Shellwords.escape(credentials['username'])
+  escaped_password = Shellwords.escape(credentials['password'])
+
+  dump_command = "mariadb-dump -u #{escaped_username} --password=#{escaped_password} --databases taskcentral > res/mariadb_dump.sql"
 
   rsync_exists = system("command -v rsync >/dev/null 2>&1")
 
@@ -13,6 +21,11 @@ $CRON_SCHEDULER.cron "0 2 * * *" do
   else
     command = "cp -r #{src}/ #{dst}/"
   end
+
+  puts Time.now
+  puts "CRON JOB: Running #{dump_command}"
+
+  system(dump_command)
 
   puts Time.now
   puts "CRON JOB: Running #{command}"
@@ -26,3 +39,9 @@ $CRON_SCHEDULER.cron "0 2 * * *" do
   end
 end
 
+# Backup res to res.swp every 2 AM PDT (4 AM CDT)
+$CRON_SCHEDULER.cron "0 2 * * *" do
+  cron_backup_res
+end
+
+cron_backup_res
