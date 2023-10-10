8af45b1d7d3298d18f77d1e5c2b15030d91cc58a
SitanHuang <978494543.qq54@gmail.com>
2023-10-10 12:22:07 -0700

    improve compiler flags

--- src/middleware/charset.rs
+++ src/middleware/charset.rs
@@ -17,7 +17,7 @@ pub async fn append_charset_utf8<B>(
     if let Some(content_type) = res.headers_mut().get_mut(CONTENT_TYPE) {
         let value = content_type.to_str().unwrap_or_default();
         if !value.contains("charset=utf8") &&
-           !value.contains("charset=utf-8"){
+           !value.contains("charset=utf-8") {
             let new_val = format!("{}; charset=utf-8", value);
             *content_type = HeaderValue::from_str(&new_val).unwrap();
         }
