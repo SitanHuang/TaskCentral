93624b0e7c750d8d5d5ecfacc9e884f5b27e14bc
SitanHuang <978494543.qq54@gmail.com>
2023-11-24 12:52:16 -0800

    ui: move Snoozed filter checkbox to Status group

--- app/public/client/libs/index.js
+++ app/public/client/libs/index.js
@@ -1,8 +1,9 @@
-const RELEASE_DATE = '20231107';
+const RELEASE_DATE = '20231124';
 
 const RELEASE_NOTES = `
  Release notes:
-1. Add Firefox support for themed scrollbars
+1. Moved Snoozed filter checkbox to Status group to better reflect its behavior
+2. Fixed ui_filter_save causing target_provider not found error
 `;
 
 if (localStorage.last_release && localStorage.last_release != RELEASE_DATE.toString()) {
