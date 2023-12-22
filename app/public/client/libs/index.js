63c09c18a645f8b86567c9313fff12af5b5090ca
SitanHuang <978494543.qq54@gmail.com>
2023-12-22 11:12:26 -0800

    feat: tab blinking on pomodoro end

--- app/public/client/libs/index.js
+++ app/public/client/libs/index.js
@@ -1,9 +1,8 @@
-const RELEASE_DATE = '20231124';
+const RELEASE_DATE = '20231222';
 
 const RELEASE_NOTES = `
  Release notes:
-1. Moved Snoozed filter checkbox to Status group to better reflect its behavior
-2. Fixed ui_filter_save causing target_provider not found error
+1. Add preliminary visual indicator when Pomodoro runs out
 `;
 
 if (localStorage.last_release && localStorage.last_release != RELEASE_DATE.toString()) {
--- app/public/client/libs/ui/timer.js
+++ app/public/client/libs/ui/timer.js
@@ -6,6 +6,8 @@ let _timer_stamp;
 
 let _timer_pomodoro_start;
 
+let _timer_blink_cancel_callback = () => {};
+
 function timer_start_task(task) {
   // does nothing if unset
   clearInterval(_timer_interval_id);
@@ -31,6 +33,9 @@ function timer_start_task(task) {
         .text(timeIntervalString(timestamp(), pom_end));
     } else {
       // time's up
+
+      _timer_blink_cancel_callback = _timer_blink_title();
+
       _timer_pomodoro_beep_bursts();
       setTimeout(_timer_pomodoro_beep_bursts, 400 + 200);
       setTimeout(_timer_pomodoro_beep_bursts, (400 + 200) * 2);
@@ -70,6 +75,8 @@ function _timer_pomodoro_stop() {
 function timer_stop_task(task) {
   task_pause(back.data.tasks[back.data.started]);
 
+  _timer_blink_cancel_callback();
+
   _timer_pomodoro_stop();
 
   // does nothing if unset
@@ -101,4 +108,63 @@ function _timer_pomodoro_beep_bursts() {
 
   oscillator.start();
   oscillator.stop(_timer_audiocontext.currentTime + 0.2);
+}
+
+function _timer_blink_title() {
+  const originalTitle = document.title;
+  const favicon = document.querySelector("link[rel~='icon']");
+  const newFavicon = document.createElement("link");
+  const originalImage = favicon.href;
+  // 1px red square
+  const redSquare = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdj+M/A8B8ABQAB/6Zcm10AAAAASUVORK5CYII=";
+
+  console.debug('blink');
+
+  let blinkSeq = 0;
+  let blinking = setInterval(() => {
+    switch (blinkSeq++ % 6) {
+      case 0:
+        favicon.href = redSquare;
+        document.title = "Time's up!";
+        break;
+      case 1:
+        favicon.href = originalImage;
+        document.title = "Time's up!!";
+        break;
+      case 2:
+        favicon.href = redSquare;
+        document.title = "Time's up!!!";
+        break;
+      case 3:
+        favicon.href = originalImage;
+        document.title = "Time's up!!!!";
+        break;
+      case 4:
+        favicon.href = redSquare;
+        document.title = "Time's up!!!!!";
+        break;
+      case 5:
+        favicon.href = originalImage;
+        document.title = "\u200E"; // invisible placeholder
+        break;
+    }
+  }, 400);
+
+  let _isCanceled = false;
+  function cancel() {
+    if (_isCanceled) return;
+
+    clearInterval(blinking);
+    document.title = originalTitle;
+    favicon.href = originalImage;
+
+    _isCanceled = true;
+  }
+
+  // stop after 10 seconds
+  setTimeout(() => {
+    cancel();
+  }, 10000);
+
+  return cancel;
 }
